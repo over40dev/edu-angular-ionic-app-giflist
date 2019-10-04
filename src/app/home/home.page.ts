bb79369ca902f156672f93b84bc95eecbf5ed9bd
Cogentx <cogentx@live.com>
2019-10-04 15:46:43 -0230

    giflist-L03-initial form setup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Plugins } from '@capacitor/core';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { SettingsPage } from '../settings/settings.page';
+import { DataService } from '../services/data.service';
+import { RedditService } from '../services/reddit.service';
+
+const { Browser, Keyboard } = Plugins;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public subredditForm: FormGroup;
 
-  constructor() {}
+  constructor(
+    private modalCtrl: ModalController,
+    private dataService: DataService,
+    private redditService: RedditService
+  ) {
+    this.subredditForm = new FormGroup({
+      subredditControl: new FormControl(''),
+    });
+  }
 
+  ngOnInit() {}
+
+  showComments(post): void {}
+
+  openSettings(): void {}
+
+  playVideo(e, post): void {}
+
+  loadMore(): void {}
 }
--- src/app/services/reddit.service.ts
+++ src/app/services/reddit.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DataService } from './data.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RedditService {
 
-  constructor() { }
+  public settings = {
+    perPage: 10,
+    subReddit: 'gifs',
+    sort: '/hot',
+  };
+
+  public posts: any[];
+  public loading: boolean;
+  private page: number;
+  private after: string;
+  private moreCount: number;
+
+  constructor(private http: HttpClient, private dataService: DataService) {
+    this.posts = [];
+    this.loading = false;
+    this.page = 1;
+    this.moreCount = 0;
+  }
+
+  load(): void {
+
+  }
+
+  fetchData(): void {
+
+  }
+
+  nextPage(): void {
+
+  }
+
+  resetPosts(): void {
+
+  }
+
+  changeSubReddit(subreddit): void {
+
+  }
+
+
+
+
+
 }
